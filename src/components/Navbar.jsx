c01397e407488e1d94d65de59874d8dc0ef33a9d
eminc Marques Batista <eminc7@hotmail.com>
2025-04-23 16:15:45 -0300

    projeto inteiro

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <img src="src/assets/react.svg" alt="logo da react" />
+        <span>React Facts</span>
+      </nav>
+    </header>
+  );
+}
